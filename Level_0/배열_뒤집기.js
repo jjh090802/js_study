e1b0cc6153ab5b8995c4ed90bd4ad471ee027482
전재현 <j01092014477@gmail.com>
2026-06-17 15:28:57 +0900

    [프로그래머스] Level_0 배열 뒤집기

---
+++ Level_0/배열_뒤집기.js
@@ -0,0 +1,8 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120821
+
+function solution(num_list) {
+    return num_list.reverse()
+}
+
+//reverse라는 메서드에 대해서 처음 알게되었다.
+//reverse라는 메서드는 배열을 뒤집는 함수이다.
