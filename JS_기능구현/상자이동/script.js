6e9620c3fdb7a7ddba7e0e5e5677c398bae67776
전재현 <j01092014477@gmail.com>
2026-07-11 17:19:37 +0900

    상자이동 폴더 추가 (html, css, js)

---
+++ JS_기능구현/상자이동/script.js
@@ -0,0 +1,32 @@
+const box = document.querySelector(".box")
+
+const maxTop = window.innerHeight - box.offsetHeight;
+const maxLeft = window.innerWidth - box.offsetWidth;
+
+let topPos = maxTop / 2;
+let leftPos = maxLeft / 2;
+
+const speed = 20;
+
+box.style.top = topPos + 'px';
+box.style.left = leftPos + 'px';
+
+window.addEventListener('keydown', (event) => {
+    const key = event.key;
+
+    if(key === 'ArrowUp' && topPos - speed >= 0){
+        topPos -= speed;
+    } else if(key === 'ArrowDown' && topPos + speed <= maxTop){
+        topPos += speed;
+    } else if(key === 'ArrowLeft' && leftPos - speed >= 0){
+        leftPos -= speed;
+    } else if(key === 'ArrowRight' && leftPos + speed <= maxLeft){
+        leftPos += speed;
+    } else{
+        return ;
+    }
+
+    event.preventDefault();
+    box.style.top = topPos + 'px';
+    box.style.left = leftPos + 'px';
+})
