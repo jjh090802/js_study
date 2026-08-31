2b1e9a86f06b05c567f23c10173913762fd2ca14
jjh090802 <j01092014477@gmail.com>
2026-08-31 15:07:56 +0900

    [lv0] 문자열 정수변환

---
+++ programmers/java/lv0/문자열을_정수로_변환하기.java
@@ -0,0 +1,11 @@
+//문제 : 문자열을 정수로 변환하기 (lv0)
+//https://school.programmers.co.kr/learn/courses/30/lessons/181848
+// 요약: 문자열을 정수형으로 변환하여 return
+
+class Solution {
+    public int solution(String n_str) {
+        int answer = Integer.parseInt(n_str);
+        
+        return answer;
+    }
+}
